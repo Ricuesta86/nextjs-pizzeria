6583756da5b7f991e777b82bcba41cc579de7c51
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2024-02-15 07:06:14 -0500

    Add Hero

---
+++ app/components/hero/sectionimage.tsx
@@ -0,0 +1,35 @@
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function SectionImage() {
+  const [imageId, setImageId] = useState("/biryani2.png");
+  const imageList = [
+    { id: 1, image: "/biryani2.png" },
+    { id: 2, image: "/biryani3.png" },
+    { id: 3, image: "/biryani5.png" },
+  ];
+  const changeImage=(image:string)=>{
+    setImageId(image)
+  }
+  return (
+    <>
+      <div className="flex justify-center items-center h-[300px] sm:h-[450px] overflow-hidden">
+        <Image
+          src={imageId}
+          alt="Image section"
+          width={450}
+          height={450}
+          className="w-[330px] sm:w-[450px] mx-auto spin"
+        />
+      </div>
+      <div className="flex lg:flex-col lg:top-1/2 lg:-translate-y-1/2 lg:py-2 justify-center gap-4 absolute bottom-[0px] lg:-right-10 bg-white/30 rounded-full">
+        {imageList.map((item)=>(
+            <Image key={item.id} src={item.image} width={450} height={450} alt="Section imagen" className="max-w-[80px] h-[80px] object-contain inline-block hover:scale-105 duration-200"
+            onClick={()=>changeImage(item.image)}
+            />
+        ))}
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "./components/hero/hero";
 import Navbar from "./components/navbar/navbar";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-
+      <Hero />
     </>
   );
 }
